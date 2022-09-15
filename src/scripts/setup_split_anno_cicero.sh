be9ed3122823538e4f6acf7cbcd2cbbced743d12
cgallops <cgallops@stjude.org>
2022-09-15 15:21:53 -0500

    back slash again

--- src/scripts/setup_split_anno_cicero.sh
+++ src/scripts/setup_split_anno_cicero.sh
@@ -326,9 +326,9 @@ while read case_bam
    cat \$outdir/annotated.fusion.txt \$outdir/annotated.internal.txt > \$outdir/annotated.all.txt
    
    tail -n +2 \$outdir/annotated.all.txt | split -a 5 -d -l 250 - \$outdir/annotated.all.txt.
-   for x in $(ls \$outdir/annotated.all.txt.*)
+   for x in \$(ls \$outdir/annotated.all.txt.*)
    do
-     echo "$(cat <(head -n1 \$outdir/annotated.all.txt) $x)" > $x
+     echo "\$(cat <(head -n1 \$outdir/annotated.all.txt) \$x)" > \$x
    done
    
 done < $RUN_DIR/config.txt
